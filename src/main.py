9575e280f7126807d821e81879c02f17667cbc50
mi-dy <mindaugasdy@gmail.com>
2026-02-09 12:56:45 +0200

    Added split_nodes_link

--- src/main.py
+++ src/main.py
@@ -101,6 +101,37 @@ def split_nodes_image(old_nodes):
 
     return new_nodes
 
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        links = extract_markdown_links(node.text)
+        if links == []:
+            new_nodes.append(node)
+            continue
+
+        for link_alt, link in links:
+            sections = node.text.split(f"[{link_alt}]({link})", 1)
+            
+            if len(sections) > 1:
+                if sections[0] != "":
+                    new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+                new_nodes.append(TextNode(link_alt, TextType.LINK, link))
+                node.text = sections[1]
+
+        if node.text != "":
+            new_nodes.append(TextNode(node.text, TextType.TEXT))
+
+    return new_nodes
+
+
 def split_nodes_link(old_nodes):
 
 if __name__ == "__main__":
